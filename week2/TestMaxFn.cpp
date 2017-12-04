5bd87d501a4ca711e16c67cb5dc674f36bc3de49
dilmanpreet <dilmanpreet1@gmail.com>
2017-12-04 13:39:59 -0800

    update Testmaxfn.cpp

--- week2/TestMaxFn.cpp
+++ week2/TestMaxFn.cpp
@@ -1,5 +1,27 @@
-       #include <iostream>
+#include <iostream>
 using namespace std;
+
+// Function prototype
+int max(int num1, int num2);
+double max(double num1, double num2);
+double max(double num1, double num2, double num3);
+
+int main()
+{
+  // Invoke the max function with int parameters
+  cout << "The maximum between 3 and 4 is " <<
+    max(3, 4) << endl;
+
+  // Invoke the max function with the double parameters
+  cout << "The maximum between 3.0 and 5.4 is "
+    << max(3.0, 5.4) << endl;
+
+  // Invoke the max function with three double parameters
+  cout << "The maximum between 3.0, 5.4, and 10.14 is "
+    << max(3.0, 5.4, 10.14) << endl;
+
+  return 0;
+}
 
 // Return the max between two int values 
 int max(int num1, int num2)
@@ -23,21 +45,5 @@ double max(double num1, double num2)
 double max(double num1, double num2, double num3)
 {
   return max(max(num1, num2), num3);
-}
-
-int main()
-{
-  // Invoke the max function with int parameters
-  cout << "The maximum between 3 and 4 is " << max(3, 4) << endl;
-
-  // Invoke the max function with the double parameters
-  cout << "The maximum between 3.0 and 5.4 is "
-    << max(3.0, 5.4) << endl;
-
-  // Invoke the max function with three double parameters
-  cout << "The maximum between 3.0, 5.4, and 10.14 is "
-    << max(3.0, 5.4, 10.14) << endl;
-
-  return 0;
 }
 
